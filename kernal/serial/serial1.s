44d4767c9433f3f31666e5b4a52f58f419e36ffb
Maciej Witkowiak <ytm@elysium.pl>
2022-11-03 22:39:46 +0100

    my own serial $1CD5 for GEOOffice87 or cbmfiles

--- kernal/serial/serial1.s
+++ kernal/serial/serial1.s
@@ -17,19 +17,10 @@
 ASSERT_IN_BANK0
 
 SerialNumber:
-.ifdef wheels
-	.word $DF96
-.else
-.ifdef bsw128
-	; This matches the serial in the cbmfiles.com GEOS128.D64
-	.word $FD8D
+.ifdef atari
+	.word $1CD5	; YTM's cracked GEOS
 .else
 	; This matches the serial in the cbmfiles.com GEOS64.D64
 	.word $58B5
-.endif
-.endif
-
-.ifndef wheels_size
-	.byte $FF ; ???
 .endif
 
